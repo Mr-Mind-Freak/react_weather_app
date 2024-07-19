8ed26c6bbcdc938d56d1660d011d825533bce143
Mr-Mind-Freak <navaneethan2482@gmail.com>
2024-07-19 11:24:56 +0530

    UI improved and responsiveness added

--- src/App.js
+++ src/App.js
@@ -1,52 +1,86 @@
-import React,{useContext} from 'react';
+import React, {useContext, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Data from './DataContext';
-import Header from './components/Header';
-import Main from './components/Main';
+import { Modal, Button } from "react-bootstrap";
 
 const App = () => {
-  const {weatherWord} = useContext(Data);
-  let appClass = "app";
-  switch (weatherWord.toLowerCase()) {
-    case "clouds":
-    case "mist":
-      appClass += " mist";
-      break;
-    case "clear":
-      appClass += " clear";
-      break;
-    case "drizzle":  
-    case "rain":
-      appClass += " rain";
-      break;
-    case "fog":
-      appClass += " fog";
-      break;
-    case "haze":  
-    case "frosty":
-      appClass += " frosty";
-      break;
-    case "sunny":
-      appClass += " sunny";
-      break;
-    case "snow":
-      appClass += " snow";
-      break;
-    case "thunderstorm":
-      appClass += " thunderstorm";
-      break;
-    default: 
-      appClass += " normal";
-      break;  
+  const { location, setLocation, temp, date, extractDate, weather, iconpath, wind, humidity, weatherData, handleInput,showModal, setShowModal } = useContext(Data);
+  const [celcious, setCelcious] = useState(true);
+  let non_active = 'm-0 w-50 py-3 text-center text-capitalize text-light rounded-pill';
+  let active = 'm-0 w-50 py-3 text-center text-capitalize text-light rounded-pill measurement';
+  const today = extractDate(date);
+
+  const handleLocationChange = (e)=> {
+    setLocation(e.target.value);
   }
-  
-  
-  
+
+  const handleClose = () => setShowModal(false);
+   
   return (
-    <div className={appClass}>
-      <article>
-        <Header />
-        <Main />
-      </article>
+    <div className='container pt-3'>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className='text-danger'> ERROR </Modal.Title>
+        </Modal.Header>
+        <Modal.Body> Please Enter the Correct City Name </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <section className='row d-flex justify-content-between'>
+        <div className='col-2 col-md-1 p-0 d-flex justify-content-between align-items-center rounded-pill' style={{backgroundColor : 'rgb(255,255,255,0.16)'}}>
+          <p  className={ celcious ? non_active : active} onClick={()=>{setCelcious(false)}}> f </p>
+          <p className={ celcious ? active : non_active } onClick={()=>{setCelcious(true)}}> c </p>
+        </div>
+        <div className='col-10 col-md-4 d-flex align-items-center justify-content-evenly'>
+          <img src={require('./assets/loc_icon.png')} alt='loc_icon' width={30} height={30} className='d-block'/>
+          <input type='text' id='location' name='location' value={location} size={location.length+3} onChange={handleLocationChange} onKeyDown={handleInput} className='text-light h5 inputData' style={{backgroundColor : 'transparent', border:'none'}}/>
+          <img src={require('./assets/search_icon.png')} alt='search_icon' width={40} height={40} className='d-block'/>
+        </div>
+      </section>
+      <section className='row mt-md-2 mt-lg-5'>
+        <div className='col-12 col-md-6 d-flex flex-wrap flex-md-nowrap align-items-center justify-content-evenly'>
+          <div className=''>
+            <img src={iconpath} alt='weather icon' width={200} height={200} className='d-block mx-auto'/>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light text-center mt-3'> {weather} </p>
+            <div className='d-flex justify-content-between'>
+              <div className='d-flex my-auto'>
+                <img src={require('./assets/wind-direction-icon.png')} alt='icon' width={20} height={20} className='d-block me-1'/>
+                <p className='text-light me-2'> {`Wind: ${wind}Km/h`} </p>
+              </div>
+              <div className='d-flex my-auto'>
+                <img src={require('./assets/hum.png')} alt='icon' width={20} height={20} className='d-block me-1'/>
+                <p className='text-light me-2'> {`Humidity: ${humidity}%`} </p>
+              </div>
+            </div>
+            <p className='text-white text-capitalize border border-white rounded text-center' style={{fontSize:'2.2rem'}}> {location} </p>
+          </div>
+          <div className=''>
+            <p style={{fontSize:'5rem', fontWeight : 200}} className='text-light'> { celcious ? temp : temp*(9/5)+32} <sup> {celcious?'C':'F'} </sup> </p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> {`${today.date}th ${today.month}, ${today.year}`} </p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> {today.day} </p>
+          </div>
+        </div>
+        <div className='col-12 col-md-6 d-flex justify-content-evenly align-items-center border border-white rounded' style={{backgroundColor: 'rgb(255,255,255,0.20)'}} >
+          <div>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> Feels Like</p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> Temp Max </p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> Temp Min </p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> Pressure </p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> Wind Deg </p>
+          </div>
+          <div>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> {weatherData.main?.feels_like ? weatherData.main.feels_like : '30'} </p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> {weatherData.main?.temp_max ? weatherData.main.temp_max : '30'} </p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> {weatherData.main?.temp_min ? weatherData.main.temp_min : '30'} </p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> {weatherData.main?.pressure ? weatherData.main.feels_like : '30'} </p>
+            <p style={{fontSize:'1.5rem', fontWeight : 200}} className='text-light'> {weatherData.wind?.deg ? weatherData.main.feels_like : '30'} </p>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
--- src/DataContext.js
+++ src/DataContext.js
@@ -1,41 +1,161 @@
-import { createContext, useState} from 'react';
+import { createContext, useState, useEffect} from 'react';
 
 const Data = createContext({});
 
+const extractDate = (date) => {
+  let today = {};
+  today.date = date.getDate();
+  switch (date.getDay()) {
+    case 0:
+      today.day = 'Monday';
+      break;
+    case 1:
+      today.day = 'Tuesday';
+      break;
+    case 2:
+      today.day = 'Wednesday';
+      break;
+    case 3:
+      today.day = 'Thursday';
+      break;
+    case 4:
+      today.day = 'Friday';
+      break;
+    case 5:
+      today.day = 'Saturday';
+      break;
+    case 6:
+      today.day = 'Sunday';
+      break;
+    default:
+      today.day = 'Sunday';
+      break;
+  }
+  switch (date.getMonth()) {
+    case 0:
+      today.month = 'January'
+      break;
+    case 1:
+      today.month = 'February'
+      break;
+    case 2:
+      today.month = 'March'
+      break;
+    case 3:
+      today.month = 'April'
+      break;
+    case 4:
+      today.month = 'May'
+      break;
+    case 5:
+      today.month = 'June'
+      break;
+    case 6:
+      today.month = 'July'
+      break;
+    case 7:
+      today.month = 'Auguest'
+      break;
+    case 8:
+      today.month = 'Semptember'
+      break;
+    case 9:
+      today.month = 'October'
+      break;
+    case 10:
+      today.month = 'November'
+      break;
+    case 11:
+      today.month = 'December'
+      break;
+    default:
+      today.month = 'Auguest'
+      break;
+  }
+  today.year = date.getFullYear();
+  return today;
+}
+
 export const DataProvider = ({ children}) => {
-  let [ location, setlocation] = useState('');
-  let [ weather, setweather ] = useState(null);
-  let [ weatherWord, setWeatherWord] = useState('normal');
+  let [ location, setLocation] = useState('Delhi');
+  const [ weatherData, setWeatherData ] = useState({});
+  let [ temp, setTemp ] = useState(0);
+  let [ date, setDate] = useState(new Date());
+  const [ wind, setWind ] = useState(10);
+  const [ humidity, setHumidity ] = useState(40);
+  const [ weather, setWeather ] = useState('clear');
+  const [ iconpath, setIconpath ] = useState(require('./assets/clear.png'));
+  const [showModal, setShowModal] = useState(false);
+
   const url = {
     key : "5426b661aa9ed513a01f917a84f5b6be",
     base: "https://api.openweathermap.org/data/2.5/"
   }
 
-  const collectData = async ( event ) => {
-    if ( event.key === 'Enter') {
-      try {
-        const responce = await fetch(`${url.base}weather?q=${location}&units=metric&APPID=${url.key}`);
-        const result = await responce.json();
-        setweather(result);
-        setWeatherWord(result.weather[0].main);
-        setlocation("");
-      }
-      catch (err) {
-        window.alert("Invalid Location....Please enter correct location");
-        setlocation("");
-      }
+  const extractIcon = (wid) => {
+    switch (wid) {
+      case '11d':
+        setIconpath(require('./assets/thunder.png'));
+        break;
+      case '09d':
+        setIconpath(require('./assets/drizzle.png'));
+        break;
+      case '10d':
+        setIconpath(require('./assets/rain.png'));
+        break;
+      case '13d':
+        setIconpath(require('./assets/snow.png'));
+        break;
+      case '50d':
+        setIconpath(require('./assets/haze.png'));
+        break;
+      case '01d':
+      case '01n':
+        setIconpath(require('./assets/clear.png'));
+        break;
+      case '02d':
+      case '02n':
+      case '03d':
+      case '03n':
+      case '04d':
+      case '04n':
+        setIconpath(require('./assets/cloud.png'));
+        break;
+      default:
+        setIconpath(require('./assets/clear.png'));
+        break;
     }
   }
 
-  const handleInputBox = ( event ) => {
-    document.getElementById('input_box').classList.toggle('active');
+  const fetchData = async() =>{
+    try {
+      const res = await fetch(`${url.base}weather?q=${location}&units=metric&APPID=${url.key}`);
+      const data = await res.json();
+      setWeatherData(data);
+      setTemp(Math.round(data.main.temp));
+      setWind(Math.ceil(data.wind.speed));
+      setHumidity(data.main.humidity);
+      setWeather(data.weather[0].main);
+      extractIcon(data.weather[0].icon);
+    } catch (error) {
+      console.error(error.stack);
+      setShowModal(true);
+    }
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
+  const handleInput = ( event ) => {
+    if ( event.key === 'Enter') {
+      fetchData();
+    }
   }
 
   return(
     <Data.Provider value={{
-      location, setlocation, 
-      weather, setweather,
-      handleInputBox, collectData,weatherWord
+      location, setLocation, temp, date, extractDate, wind, humidity, weather, iconpath, weatherData, handleInput,showModal, setShowModal
     }}>
       {children}
     </Data.Provider>
